f1575fa05448473099c410c04c5e2507fef937b2
Md Tanbin Hossain Himel <68559342+Tanbinhimel@users.noreply.github.com>
2021-08-02 03:55:11 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router";
 import "./App.css";
-import NavBar from "./component/common/navbar";
+import Navbar from "./components/common/navbar";
+import Login from "./components/login";
+import Products from "./components/products";
+import NotFound from "./components/not-found";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <h1 className="display-1">Display 1</h1>
-        <h1 className="display-2">Display 2</h1>
-        <h1 className="display-3">Display 3</h1>
-        <h1 className="display-4">Display 4</h1>
+      <div className="App">
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/products" component={Products} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="products" />
+            <Redirect to="/not-found" />
+          </Switch>
+          <h1 className="display-1">Display 1</h1>
+          <h1 className="display-2">Display 2</h1>
+          <h1 className="display-3">Display 3</h1>
+          <h1 className="display-4">Display 4</h1>
+        </main>
       </div>
     );
   }
---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" expand="md" variant="dark">
+        <Container className="mx-2">
+          <Link className="navbar-brand" to="/">
+            Ecom-app
+          </Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink className="nav-link" to="/products">
+                Products
+              </NavLink>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
