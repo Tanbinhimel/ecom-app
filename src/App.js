e3820682d2ca904308ae1fc80dcadf359b46f3bd
Md Tanbin Hossain Himel <68559342+Tanbinhimel@users.noreply.github.com>
2021-08-02 02:30:17 +0600

    commited last change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./component/common/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1 className="display-1">Display 1</h1>
+        <h1 className="display-2">Display 2</h1>
+        <h1 className="display-3">Display 3</h1>
+        <h1 className="display-4">Display 4</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
