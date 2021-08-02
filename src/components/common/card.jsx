51f7cca68ccf3435c31f076f6129f18417e6af02
Md Tanbin Hossain Himel <68559342+Tanbinhimel@users.noreply.github.com>
2021-08-03 01:09:16 +0600

    1

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from "react";
 import "./card.css";
 class Card extends Component {
+  // renderElement() {}
+
   render() {
+    const { cardImageURL, cardBody } = this.props;
     return (
       <div class="col my-2">
         <div className="card h-30 customCard">
-          <img
-            class="card-img-top"
-            src="https://picsum.photos/1000/1000"
-            alt="Card image cap"
-          />
-          <div class="card-body">
-            <p class="card-element card-title">Card title</p>
-            <p class="card-element">current price</p>
-            <p class="card-element">old price</p>
-            <p class="card-element">rating</p>
-          </div>
+          <img class="card-img-top" src={cardImageURL} alt="product image" />
+          <div class="card-body">{cardBody.map((component) => component)}</div>
         </div>
       </div>
     );
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,47 +1,44 @@
 import React, { Component } from "react";
 import Card from "./common/card";
+import { getProducts } from "../services/productServices";
 
 class Products extends Component {
+  state = {
+    products: [],
+  };
+
+  cardBody = (product) => [
+    <p className="card-element card-title">{product.productName}</p>,
+    <p className="card-element highlighted">
+      ${this.calculateNewPrice(product.price, product.discount)}
+    </p>,
+    <p className="card-element non-highlighted">
+      <del>${product.price.toFixed(2)}</del>
+    </p>,
+  ];
+
+  componentDidMount() {
+    const products = getProducts();
+    this.setState({ products });
+  }
+
+  calculateNewPrice(price, discount) {
+    const discountAmount = (price * discount) / 100;
+    return (price - discountAmount).toFixed(2);
+  }
+
   render() {
+    const { products } = this.state;
     return (
       <div>
         <h1>products</h1>
         <div className="row">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {products.map((product) => (
+            <Card
+              cardImageURL={"https://picsum.photos/500/500"}
+              cardBody={this.cardBody(product)}
+            />
+          ))}
         </div>
       </div>
     );
